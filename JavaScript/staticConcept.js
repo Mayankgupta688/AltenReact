1c5e7c2fc359946b7517e06791f90090359abd65
MayankGupta <mayank.gupta.6.88@gmail.com>
2021-04-09 20:58:15 +0530

    Changes

---
+++ JavaScript/staticConcept.js
@@ -0,0 +1,34 @@
+class Employee {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    getName() {
+        console.log(this.name);
+    }
+
+    getAge() {
+        console.log(this.age)
+    }
+
+    showData() {
+        this.getAge();
+    }
+}
+
+Employee.prototype.randomValue = 20;
+debugger;
+var emp = new Employee("Mayank Gupta", 30);
+
+console.log(emp.name);
+
+var empTwo = new Employee("Anshul Gupta", 35)
+console.log(empTwo.name)
+
+emp.showData();
+empTwo.showData()
+
+Employee.prototype.showData = () => {
+    console.log("Sample")
+}
